39cc361b9d2c7a4a9111ab632accc416e0bbf868
Clay Dunston <dunstontc@gmail.com>
2018-02-14 20:29:00 -0600

    Fiddling with hammerspoon. May have to be Karabiner

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -1,5 +1,17 @@
+-- Thanks Lars -- https://github.com/webpro/dotfiles/blob/master/etc/hammerspoon
+
+mash = {"⌘", "⌥", "⌃"}
+-- hyper = {"cmd", "alt", "ctrl"}
+
+require "apps"
+
+-- hs.hotkey.bind(mash, "r", function() hs.reload(); end)
+-- hs.hotkey.bind(mash, "w", function() hs.eventtap.keyStrokes('¯\\_(ツ)_/¯'); end)
+-- hs.hotkey.bind(mash, "a", function() hs.caffeinate.lockScreen(); end)
+-- hs.alert("Hammerspoon config loaded")
+
 --- Reload Hammerspoon configuration
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
+hs.hotkey.bind(mash, "R", function()
   hs.reload()
   hs.notify.new({title="Hammerspoon", informativeText="Config reloaded"}):send()
 end)
@@ -23,119 +35,4 @@ end)
 --
 
 
-
----- More complex window movement ----
--- Nethack Movement
--- untelated to hammerspoon, but interesting
--- y   k   u
--- h       l
--- b   j   n
---
--- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Y", function()
---   local win = hs.window.focusedWindow()
---   local f = win:frame()
---
---   f.x = f.x - 10
---   f.y = f.y - 10
---   win:setFrame(f)
--- end)
---
--- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "K", function()
---   local win = hs.window.focusedWindow()
---   local f = win:frame()
---
---   f.y = f.y - 10
---   win:setFrame(f)
--- end)
---
--- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "U", function()
---   local win = hs.window.focusedWindow()
---   local f = win:frame()
---
---   f.x = f.x + 10
---   f.y = f.y - 10
---   win:setFrame(f)
--- end)
---
--- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", function()
---   local win = hs.window.focusedWindow()
---   local f = win:frame()
---
---   f.x = f.x - 10
---   win:setFrame(f)
--- end)
---
--- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "L", function()
---   local win = hs.window.focusedWindow()
---   local f = win:frame()
---
---   f.x = f.x + 10
---   win:setFrame(f)
--- end)
---
--- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "B", function()
---   local win = hs.window.focusedWindow()
---   local f = win:frame()
---
---   f.x = f.x - 10
---   f.y = f.y + 10
---   win:setFrame(f)
--- end)
---
--- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "J", function()
---   local win = hs.window.focusedWindow()
---   local f = win:frame()
---
---   f.y = f.y + 10
---   win:setFrame(f)
--- end)
---
--- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "N", function()
---   local win = hs.window.focusedWindow()
---   local f = win:frame()
---
---   f.x = f.x + 10
---   f.y = f.y + 10
---   win:setFrame(f)
--- end)
-
-
-
----- Window sizing ----
--- fetch the focused window,
--- then fetch the screen that the focused window is on,
--- fetch the frame of the screen (note that hs.screen.frame() does not include the menubar and dock, see hs.screen.fullFrame() if you need that)
--- the frame of the window to occupy the left half of the screen.
--- and set the frame of the window to occupy the left half of the screen.
---
--- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
---   local win = hs.window.focusedWindow()
---   local f = win:frame()
---   local screen = win:screen()
---   local max = screen:frame()
---
---   f.x = max.x
---   f.y = max.y
---   f.w = max.w / 2
---   f.h = max.h
---   win:setFrame(f)
--- end)
---
--- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
---   local win = hs.window.focusedWindow()
---   local f = win:frame()
---   local screen = win:screen()
---   local max = screen:frame()
---
---   f.x = max.x + (max.w / 2)
---   f.y = max.y
---   f.w = max.w / 2
---   f.h = max.h
---   win:setFrame(f)
--- end)
-
-
-
----- Multi-window layouts ----
---
 
--- linting/readme.md
+++ linting/readme.md
@@ -1,6 +1,7 @@
 ## Linting ##
 - tern
 - [eslint/eslint](https://eslint.org/docs/user-guide/configuring)
+  - [sindresorhus/eslint-plugin-unicorn](https://github.com/sindresorhus/eslint-plugin-unicorn)
 - [jshint/jshint](http://jshint.com/docs/options/)
 - [luacheck](http://luacheck.readthedocs.io/en/stable/warnings.html)
 - [amperser/proselint](https://github.com/amperser/proselint#checks)
@@ -17,3 +18,5 @@
 - gofmt
 - fixjson
 - remark
+- [DavidAnson/markdownlint](https://github.com/DavidAnson/markdownlint)
+  - [igorshubovych/markdownlint-cli](https://github.com/igorshubovych/markdownlint-cli)
