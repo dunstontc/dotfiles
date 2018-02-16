b83302824faf58bcb5bcf3ccfcf6786b18be0c9e
Clay Dunston <dunstontc@gmail.com>
2018-02-16 16:08:21 -0600

    Cleaner code still

--- linting/.eslintrc.js
+++ linting/.eslintrc.js
@@ -1,41 +1,39 @@
 module.exports = {
-  "root": "true",
-  // "plugins": ["example"],
-  "extends": "eslint:recommended",
-  "env": {
-    "browser": true,
-    "node": true,
+  'root': true,
+  // 'plugins': ['example'],
+  'extends': 'eslint:recommended',
+  'env': {
+    'browser': true,
+    'node': true,
   },
-  "settings": {
-    "sharedData": "Hello"
+  'settings': {
+    'sharedData': 'Hello'
   },
-  "parserOptions": {
-    "ecmaVersion": 2017,
-    "ecmaFeatures": {
-        "jsx": true
+  'parserOptions': {
+    'ecmaVersion': 2017,
+    'ecmaFeatures': {
+      'jsx': true
     },
-    "sourceType": "module"
+    'sourceType': 'module'
   },
-  "rules": {
-    // "comma-dangle": ["error", "always"], // I don't like this one
-    "comma-dangle": "off",
-    "curly": "error",
-    "eqeqeq": "off",
-    "id-length": ["error", { "min": 4 }],
-    "indent": ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    "no-inline-comments": "off",
-    "no-console": "off",
-    "quotes": ["warn", "single"],
-    "semi": ["error", "always"],
+  'rules': {
+    'comma-dangle': 0,
+    'curly': 2,
+    'eqeqeq': 'off',
+    'id-length': [2, {
+      'min': 4
+    }],
+    'indent': [2, 2],
+    'no-inline-comments': 0,
+    'no-console': 0,
+    'quotes': [1, 'single'],
+    'semi': [2, 'always'],
   }
-}
+};
 
 /**
- *
  * Configuring Rules:
- *     "off" or 0 - turn the rule off
- *    "warn" or 1 - turn the rule on as a warning (doesn’t affect exit code)
- *   "error" or 2 - turn the rule on as an error (exit code is 1 when triggered)
- *
+ *     'off' or 0 - turn the rule off
+ *    'warn' or 1 - turn the rule on as a warning (doesn’t affect exit code)
+ *   'error' or 2 - turn the rule on as an error (exit code is 1 when triggered)
  */
