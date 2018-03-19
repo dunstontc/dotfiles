42b52596e10dbb27c470bb249448e02c04aac673
Clay Dunston <dunstontc@gmail.com>
2018-03-19 04:33:53 -0500

    👍

--- bin/downtime.sh
+++ bin/downtime.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Courtesy of Gregory Pakosz
 # https://github.com/gpakosz/.tmux
 
@@ -6,45 +7,27 @@ _uptime() {
     *Darwin*)
       boot=$(sysctl -q -n kern.boottime | awk -F'[ ,:]+' '{ print $4 }')
       now=$(date +%s)
+      lifetime=$((now-boot))
       ;;
     *Linux*|*CYGWIN*|*MSYS*|*MINGW*)
-      boot=0
-      now=$(cut -d' ' -f1 < /proc/uptime)
+      lifetime=$(cut -d' ' -f1 < /proc/uptime)
       ;;
     *OpenBSD*)
       boot=$(sysctl -n kern.boottime)
       now=$(date +%s)
+      lifetime=$((now-boot))
       ;;
   esac
 
-  lifetime=$((now - boot))
-  # echo -n "$lifetime"
-  d=$((lifetime / 86400))
-  h=$(((lifetime / 3600) % 24))
-  m=$(((lifetime / 60) % 60))
-  # s=$(((lifetime) % 60))
+  d=$(echo "$lifetime" | awk '{print int($1 / 86400)}')
+  h=$(echo "$lifetime" | awk '{print int(($1 / 3600) % 24)}')
+  m=$(echo "$lifetime" | awk '{print int(($1 / 60) % 60)}')
   # printf "↑ ${d}d ${h}h ${m}m ${s}s"
   if [[ $d -gt 0 ]]; then
-    printf "↑ ${d}d ${h}h ${m}m"
+    printf "↑ ${d}d ${h}h %02dm" $m
   else
-    printf "↑ ${h}h ${m}m"
+    printf "↑ %01dh %02dm" $h $m
   fi
 }
-
-  # shellcheck disable=SC1004
-  # awk -v boot="$boot" -v now="$now" '
-  #   BEGIN {
-  #     uptime = now - boot
-  #     d = int(uptime / 86400)
-  #     h = int(uptime / 3600) % 24
-  #     m = int(uptime / 60) % 60
-  #     s = int(uptime) % 60
-  #
-  #     system("tmux  set -g @uptime_d " d + 0 " \\; " \
-  #                  "set -g @uptime_h " h + 0 " \\; " \
-  #                  "set -g @uptime_m " m + 0 " \\; " \
-  #                  "set -g @uptime_s " s + 0)
-  #   }'
-# }
-
 _uptime
+
