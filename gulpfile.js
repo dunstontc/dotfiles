f137ce10e732c3b644eeaabcefc969b4b9dd124c
Clay Dunston <dunstontc@gmail.com>
2018-04-11 15:13:55 -0500

    removed spacemacs settings

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@ const gulp  = require('gulp');
 const merge = require('gulp-merge-json');
 const json5 = require('gulp-json5-to-json');
 
+const vars = {
+  codeUserSettings: "/Users/clay/Library/Application Support/Code - Insiders/User",
+  codeUserSettingsEsc: "/Users/clay/Library/Application\\ Support/Code\\ -\\ Insiders/User",
+};
+
 gulp.task('default', () => { });
 
 gulp.task('code:compile-settings', () =>
@@ -10,8 +15,6 @@ gulp.task('code:compile-settings', () =>
       fileName: "settings.json",
       json5: true,
     }))
-    .pipe(json5({
-      beautify: true,
-    }))
-    .pipe(gulp.dest('./editors/vscode/settings/'))
+    .pipe(json5({beautify:true}))
+    .pipe(gulp.dest(vars.codeUserSettings))
 );
