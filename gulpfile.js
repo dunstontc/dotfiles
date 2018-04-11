29abaa497caf48e8ab503d551f4cfa681b7ac57e
Clay Dunston <dunstontc@gmail.com>
2018-04-11 15:01:15 -0500

    Added build for vscode settings

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp  = require('gulp');
+const merge = require('gulp-merge-json');
+const json5 = require('gulp-json5-to-json');
+
+gulp.task('default', () => { });
+
+gulp.task('code:compile-settings', () =>
+  gulp.src('./editors/vscode/settings/*.json5')
+    .pipe(merge({
+      fileName: "settings.json",
+      json5: true,
+    }))
+    .pipe(json5({
+      beautify: true,
+    }))
+    .pipe(gulp.dest('./editors/vscode/settings/'))
+);
