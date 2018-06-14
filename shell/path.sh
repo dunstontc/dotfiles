e1a310358d18619bc7dfb99b2328c8d8b8a8797d
Clay Dunston <dunstontc@gmail.com>
2018-06-14 14:29:55 -0500

    python settings for vscode + anaconda

--- shell/path.sh
+++ shell/path.sh
@@ -5,22 +5,23 @@ PATH="$DOTFILES/bin:$PATH"                             # Handwritten stuff
 PATH="$HOME/bin:$PATH"                                 # Handwritten stuff
 PATH=$GOBIN:$PATH                                      # Go
 PATH="$HOME/.cargo/bin:$PATH"                          # Rust
+PATH="$HOME/anaconda3/bin:$PATH"                        # Python
 PATH="$HOME/.composer/vendor/bin:$PATH"                # PHP
-PATH="/usr/local/opt/ncurses/bin:$PATH"                # Newer ncurses
 PATH="/usr/local/mysql/bin:$PATH"                      # MySQL
-PATH="/usr/local/bin:$PATH"
+PATH="/usr/local/bin:$PATH"                            # user bin before system bin
 PATH="node_modules/.bin:vendor/bin:$PATH"              # use local packages before global
 PATH="$HOME/.node_global/bin:$PATH"                    # npm installed -g
-PATH="/usr/local/go/bin:$PATH"
+PATH="/usr/local/go/bin:$PATH"                         # Go
 if [[ "$(uname -s)" == "Darwin" ]]; then
   PATH="/Applications/Visual Studio Code - Insiders.app/Contents/Resources/app/bin:$PATH"
   PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
-  PATH="/usr/local/opt/go/libexec/bin:$PATH"
-  PATH="/usr/local/opt/llvm@5/bin:$PATH"
-  PATH="$HOME/Library/Haskell/bin:$PATH"
+  PATH="/usr/local/opt/ncurses/bin:$PATH"              # Newer ncurses
+  PATH="/usr/local/opt/go/libexec/bin:$PATH"           # Go via Brew
+  PATH="/usr/local/opt/llvm@5/bin:$PATH"               # Clang
+  PATH="$HOME/Library/Haskell/bin:$PATH"               # Haskell
 else
-  PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
-  PATH="$HOME/.rbenv/bin:$PATH"
+  PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH" # GNU coreutils
+  PATH="$HOME/.rbenv/bin:$PATH"                        # Ruby
 fi
 
 export PATH
