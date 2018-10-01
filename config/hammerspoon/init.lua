3eecd39c2c653d603ed910eabc7e89d47bef6a82
Clay Dunston <dunstontc@gmail.com>
2018-10-01 16:37:15 -0500

    update hammerspoon config

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -1,9 +1,9 @@
 -- Thanks Lars -- https://github.com/webpro/dotfiles/blob/master/etc/hammerspoon
 
-mash = {"⌘", "⌥", "⌃"}
+hyper = {"⌘", "⌥", "⌃"}
 -- hyper = {"cmd", "alt", "ctrl"}
 
-require "apps"
+-- require "apps"
 
 -- hs.hotkey.bind(mash, "r", function() hs.reload(); end)
 -- hs.hotkey.bind(mash, "w", function() hs.eventtap.keyStrokes('¯\\_(ツ)_/¯'); end)
@@ -11,9 +11,13 @@ require "apps"
 -- hs.alert("Hammerspoon config loaded")
 
 --- Reload Hammerspoon configuration
-hs.hotkey.bind(mash, "R", function()
+hs.hotkey.bind(hyper, "R", function()
   hs.reload()
   hs.notify.new({title="Hammerspoon", informativeText="Config reloaded"}):send()
+end)
+
+hs.hotkey.bind(hyper, "3", function()
+  hs.application.launchOrFocus("Launchpad")
 end)
 
 
