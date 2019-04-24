77845a7c88c28284c3fb6db75d780a121ff7538d
Clay Dunston <dunstontc@gmail.com>
2019-04-23 21:21:25 -0500

    Remove jenv from path

--- shell/path.sh
+++ shell/path.sh
@@ -11,7 +11,6 @@ PATH="/usr/local/bin:$PATH"                            # user bin before system 
 PATH="node_modules/.bin:vendor/bin:$PATH"              # use local packages before global
 PATH="$HOME/.node_global/bin:$PATH"                    # npm installed -g
 PATH="/usr/local/go/bin:$PATH"                         # Go
-PATH="$HOME/.jenv/bin:$PATH"                           # https://github.com/gcuisinier/jenv
 if [[ "$(uname -s)" == "Darwin" ]]; then
   PATH="/Applications/Visual Studio Code - Insiders.app/Contents/Resources/app/bin:$PATH"
   PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
