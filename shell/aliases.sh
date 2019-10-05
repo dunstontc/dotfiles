e9349a006f04ebeef747d52bb3b4217f1755f9ec
Clay Dunston <claydunston@gmail.com>
2019-10-05 11:10:42 -0500

    Update shell aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,11 +1,5 @@
 # Be careful
 
-# Thanks brew
-# alias python="python2.7"
-# alias python3=""
-
-alias mr='rmtrash'
-alias mrd='rmdirtrash'
 alias cp='cp -iv'
 alias mv='mv -iv'
 
@@ -25,6 +19,8 @@ alias dk="docker-compose"
 alias e="exit"
 alias g="git"
 alias j="jump"
+alias m='mmake'
+alias m2p="md2pdf"
 alias rgr="ranger"
 alias td="todo.sh"
 alias todo="todo.sh"
@@ -46,25 +42,31 @@ alias pipls="pip list --format=columns"
 # Shortcuts
 alias dots='cd $DOTFILES'
 alias nv="cd ~/.config/nvim"
-alias pj="cd ~/Projects"
+alias pjs="cd ~/Projects"
 alias dl="cd ~/Downloads"
 alias doc="cd ~/Documents"
 
 # Reload zsh
 alias zr="source ~/.zshrc"
-alias ,z="\$EDITOR ~/.zshrc"
+alias ,z='$EDITOR ~/.zshrc'
 
 # Reload bash
 alias br="source ~/.bashrc"
-alias ,b="\$EDITOR ~/.bashrc"
+alias ,b='$EDITOR ~/.bashrc'
 
 # Configure Neovim
-alias ,v="\$EDITOR ~/.vimrc"
-alias ,n="\$EDITOR ~/.config/nvim/init.vim"
-
+alias ,v='$EDITOR ~/.vimrc'
+alias ,n='$EDITOR ~/.config/nvim/init.vim'
 
 # Configure tmux
-alias remux="\$EDITOR \$DOTFILES/config/.tmux.conf"
+alias remux='$EDITOR $DOTFILES/config/.tmux.conf'
+
+# Edit a file in $DOTFILES
+dotfile() {
+  file=$(find $DOTFILES -name "*" -not -path "*/.git/**" -not -path "*/plugged/**" -not -path "*/ignore/**" | fzf)
+  [ ! -z "$file" ] && $EDITOR $file
+}
+alias ,='dotfile'
 
 # Print each PATH entry on a separate line
 alias listpath='echo -e ${PATH//:/\\n}'
@@ -75,13 +77,14 @@ alias fshow="defaults write com.apple.finder AppleShowAllFiles -bool true && kil
 alias fhide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
 
 # Copy cwd to the clipboard
-alias hdp="pwd | pbcopy"
+alias hdp='echo -n $(pwd) | pbcopy'
+alias prev="history -n | tail -n1 | pbcopy"
 
 # Get our IP
 alias ipis="ifconfig -a | perl -nle\"/(\d+\.\d+\.\d+\.\d+)/ && print $1\""
 
 # Get gpg keys
-alias gitkeys="gpg --list-secret-keys --keyid-format LONG"
+alias gitkeys='gpg --list-secret-keys --keyid-format LONG'
 
 # Git:
 alias gitmsg="curl http://whatthecommit.com/index.txt"
@@ -93,7 +96,8 @@ alias gb="git branch -a"
 
 alias tgit='GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa_tcd" git push -u origin master'
 
-alias repoz="find \$(pwd) -type d -exec test -e '{}/.git' ';' -print -prune"
+# Search recursively for git repositories and choose one to cd to.
+alias repos="cd \$(find \$(pwd) -type d -exec test -e '{}/.git' ';' -print -prune | fzf)"
 
 # Not xterm-italic
 # alias ssh="TERM=xterm-256color ssh"
@@ -106,12 +110,10 @@ tree \
 -I 'node_modules|bower_components|cache|ignore|undo|dein'\
 "
 
-# alias fag="ag --nobreak --nonumbers --noheading . | fzf"
+alias frep="ag --nobreak --nonumbers --noheading . | fzf"
 
-alias ,="\$EDITOR \$(find \$DOTFILES -name \"*\" -not -path \"*/.git/**\" -not -path \"*/plugged/**\" -not -path \"*/ignore/**\" | fzf)"
-
-alias repos="cd \$(find \$(pwd) -type d -exec test -e '{}/.git' ';' -print -prune | fzf)"
-
-# alias ccat='ccat -G String="yellow" -G Keyword="blue" -G Comment="darkgray" -G Type="teal" -G Literal="blue" -G Punctuation="lightgray" -G Plaintext="lightgray" -G Tag="darkgray" -G HTMLTag="darkgray" -G HTMLAttrName="blue" -G HTMLAttrValue="yellow" -G Decimal="green"'
 alias kat='chroma -s paraiso-dark'
-alias m='mmake'
+
+# github.com/tcd/prjr
+alias pj="prjr"
+alias pjtb="prjr ls --table"
